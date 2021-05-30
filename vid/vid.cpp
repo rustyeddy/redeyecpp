5561615555f69b80e95dae7e85c7a4e2460e737b
Rusty Eddy <rusty@radianlaser.com>
2021-05-30 08:53:29 -0700

    Added trackbar when video files are being played

--- vid/vid.cpp
+++ vid/vid.cpp
@@ -6,6 +6,8 @@
 
 using namespace std;
 
+// The trackbar can only be used on saved video files, live video is
+// not supported.
 int g_slider_position = 0;
 int g_run = 1;
 int g_dontset = 0;
@@ -21,6 +23,7 @@ void onTrackbarSlide( int pos, void * ) {
     }
 }
 
+
 int main(int argc, char** argv )
 {
     if ( argc != 2 )  {
@@ -31,6 +34,7 @@ int main(int argc, char** argv )
     cv::namedWindow( "Vid Viewer", cv::WINDOW_AUTOSIZE );
     g_cap.open(argv[1]);
 
+    // Choose a more descriptive name
     int frames = (int) g_cap.get(cv::CAP_PROP_FRAME_COUNT);
     int tmpw   = (int) g_cap.get(cv::CAP_PROP_FRAME_WIDTH);
     int tmph   = (int) g_cap.get(cv::CAP_PROP_FRAME_HEIGHT);
@@ -40,14 +44,51 @@ int main(int argc, char** argv )
          << tmph << "h"
          << endl;
 
-    //cv::createTrackbar()
+    if (frames > 0) {
+        cv::createTrackbar("Position", "X", &g_slider_position, frames, onTrackbarSlide);
+    }
+
     cv::Mat frame;
     for (;;) {
-        g_cap >> frame;
-        if ( frame.empty() ) break;
 
-        cv::imshow("VID: ", frame );
-        if ( (char) cv::waitKey(33) >= 0 ) break;
+        if ( g_run ) {
+
+            g_cap >> frame;
+            if ( frame.empty() ) break;
+
+            int curpos = (int) g_cap.get(cv::CAP_PROP_POS_FRAMES);
+            g_dontset = 1;
+
+            cout << "CURPOS: " << curpos << endl;
+            
+            if (frames > 0) {
+                cv::setTrackbarPos("Postion", "X", curpos);                
+            }
+            cv::imshow("X ", frame );
+            if ( (char) cv::waitKey(33) >= 0 ) break;
+
+            g_run = -1;
+        }
+
+        char c = (char) cv::waitKey(10);
+        switch (c) {
+        case 's':
+            g_run = 1;
+            break;
+
+        case 'r':
+            g_run = -1;
+
+        case 27:
+            break;
+
+        default:
+            //cout << "What the heck was that? " << c << endl;
+            break;
+        }
+        if (false) { // make this a debug
+            cout << "g_run: " << g_run << endl;                        
+        }
     }
     return 0;
 }
