32eb19e5a4c3d2efb907155cd2744b0a56178525
Rusty Eddy <rusty@rustyeddy.com>
2021-06-18 13:23:18 -0700

    updated video and gitignore

--- vid/video.cpp
+++ vid/video.cpp
@@ -40,10 +40,8 @@ cv::Mat& Video::get_frame()
 string Video::get_tegra(int width, int height, int fps)
 {
     string tstr;
-
-    tstr = "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)" + ::to_string(width) + ", height=(int)" +
-	   ::to_string(height) + ", format=(string)NV12, framerate=(fraction)" + ::to_string(fps) +
-           "/1 ! nvvidconv flip-method=0 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
+    
+    tstr = "nvarguscamerasrc sensor_id=0 ! video/x-raw(memory:NVMM), width=(int)" + ::to_string(width) + ", height=(int)" + ::to_string(height) + ", format=(string)NV12, framerate=(fraction)" + ::to_string(fps) + "/1 ! nvvidconv flip-method=0 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
 
     cout << "TEGRA: " << tstr << endl;
     return tstr;
