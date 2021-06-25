2127efe0d64c92d34a437853c932a80c5e641010
Rusty Eddy <rusty@radianlaser.com>
2021-06-25 07:39:32 -0700

    Added a fake /api/cameras api

--- re/web.cpp
+++ re/web.cpp
@@ -2,19 +2,29 @@
 
 #include "../vendors/cpp-httplib/httplib.h"
 
-//#define CPPHTTPLIB_OPENSSL_SUPPORT
+//
+// This is here for https / ssl support
+//
+// #define CPPHTTPLIB_OPENSSL_SUPPORT
 // #include "path/to/httplib.h"
+// httplib::SSLServer svr;
+//
 
-// HTTP
+// Normal old HTTP
 httplib::Server svr;
 
-// HTTPS
-// httplib::SSLServer svr;
+void get_cameras_cb(const httplib::Request &, httplib::Response &res)
+{
+    res.set_content( "{ \"cameras\": \"10.24.10.10\"}", "text/plain" );
+}
+
 void *web_start(void *p)
 {
     svr.Get("/api/health", [](const httplib::Request &, httplib::Response &res) {
         res.set_content("{ \"health\": \"ok\" }", "text/plain");
     });
+
+    svr.Get("/api/cameras", get_cameras_cb);
 
     svr.listen("0.0.0.0", 8080);
     return NULL;
