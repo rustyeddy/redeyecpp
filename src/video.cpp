6792d169eb92918898f84630d94d7bbbe16540fa
Rusty Eddy <rusty@rustyeddy.com>
2021-08-07 14:57:49 -0700

    Origin access set correctly

--- src/video.cpp
+++ src/video.cpp
@@ -13,7 +13,7 @@ using namespace cv;
 using namespace std;
 
 std::string get_tegra_pipeline1(int width, int height, int fps) {
-    return "nvarguscamerasrc sensor_id=1 ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv flip-method=0 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
+    return "nvarguscamerasrc sensor_id=0 ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv flip-method=0 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
 }
 
 std::string gstreamer_pipeline (int capture_width, int capture_height, int display_width, int display_height, int framerate, int flip_method) {
